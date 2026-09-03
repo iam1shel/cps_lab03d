4ba068cc8d695ec2c0e7fdf97687d2de23d43e96
leonardo <leonardo.olortegui@tecsup.edu.pe>
2026-09-03 14:38:26 -0500

    Modificacion de clases en sprint-2.

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,5 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
+        // Modificacion sprint-2
 }
--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,4 +1,5 @@
 package pe.edu.tecsup.lab03.services;
 
 public class StudentService {
+    // Modificacion sprint-2
 }
