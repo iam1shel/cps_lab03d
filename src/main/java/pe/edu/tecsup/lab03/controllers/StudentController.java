62e62027ecef34806e3f68e3fbf8a6bce80e86fc
iam1shel <luzmishelrt13@gmail.com>
2026-09-03 14:39:53 -0500

    Actualizar StudentController

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+
+    public void mostrarMensaje() {
+        System.out.println("Cambio realizado en sprint-2");
+    }
 }
